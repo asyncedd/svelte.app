5336f8b446d9292938afdd4daf043eb9829dd6dc
asyncedd <isynqquwu@proton.me>
2023-06-29 07:10:54 +0900

    feat: Prettify with prettierd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,40 @@
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap");
-  :root {
-    /* --rosewater: rgb(245, 224, 220); */
-    /* --flamingo: rgb(242, 205, 205); */
-    /* --pink: rgb(245, 194, 231); */
-    /* --mauve: rgb(203, 166, 247); */
-    /* --red: rgb(243, 139, 168); */
-    /* --maroon: rgb(235, 160, 172); */
-    /* --peach: rgb(250, 179, 135); */
-    /* --yellow: rgb(249, 226, 175); */
-    /* --green: rgb(166, 227, 161); */
-    /* --teal: rgb(148, 226, 213); */
-    /* --sky: rgb(137, 220, 235); */
-    /* --blue: rgb(137, 180, 250); */
-    /* --sapphire: rgb(116, 199, 236); */
-    /* --lavender: rgb(180, 190, 254); */
-    --base: rgb(30, 30, 46);
-    /* --mantle: rgb(24, 24, 37); */
-    /* --crust: rgb(17, 17, 27); */
-    --text: rgb(205, 214, 244);
-  }
-
-  .background {
-    background-color: var(--base);
-  }
-
-  .white_text {
-    color: var(--text);
-    text-align: center;
-    font-family: "Noto sans";
-    font-display: swap;
-  }
-</style>
-
 <div class="white_text">
-  <body class="background">
-    <h1>hi</h1>
-  </body>
+	<body class="background">
+		<h1>hi</h1>
+	</body>
 </div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+	:root {
+		/* --rosewater: rgb(245, 224, 220); */
+		/* --flamingo: rgb(242, 205, 205); */
+		/* --pink: rgb(245, 194, 231); */
+		/* --mauve: rgb(203, 166, 247); */
+		/* --red: rgb(243, 139, 168); */
+		/* --maroon: rgb(235, 160, 172); */
+		/* --peach: rgb(250, 179, 135); */
+		/* --yellow: rgb(249, 226, 175); */
+		/* --green: rgb(166, 227, 161); */
+		/* --teal: rgb(148, 226, 213); */
+		/* --sky: rgb(137, 220, 235); */
+		/* --blue: rgb(137, 180, 250); */
+		/* --sapphire: rgb(116, 199, 236); */
+		/* --lavender: rgb(180, 190, 254); */
+		--base: rgb(30, 30, 46);
+		/* --mantle: rgb(24, 24, 37); */
+		/* --crust: rgb(17, 17, 27); */
+		--text: rgb(205, 214, 244);
+	}
+
+	.background {
+		background-color: var(--base);
+	}
+
+	.white_text {
+		color: var(--text);
+		text-align: center;
+		font-family: 'Noto sans';
+		font-display: swap;
+	}
+</style>
