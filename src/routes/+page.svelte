ffe0d6a45ccee64c2f4013f6b87c5dd465ddccf0
asyncedd <isynqquwu@proton.me>
2023-06-29 17:10:52 +0900

    feat(tailwindcss): specify a text color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="white_text">
 	<body class="bg-ctp-base background">
-		<h1 class="mx-auto text-center text-5xl antialiased leading-relaxed">hi</h1>
+		<h1 class="text-ctp-sky mx-auto text-center text-5xl antialiased leading-relaxed">hi</h1>
 	</body>
 </div>
 
