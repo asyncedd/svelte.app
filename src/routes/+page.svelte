7195099404d5bd3352d860be03990a36f81d82f0
asyncedd <isynqquwu@proton.me>
2023-06-29 17:08:06 +0900

    feat(tailwindcss): use catppuccin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="white_text">
-	<body class="background">
+	<body class="bg-ctp-base background">
 		<h1 class="mx-auto text-center text-5xl antialiased leading-relaxed">hi</h1>
 	</body>
 </div>
@@ -10,30 +10,6 @@
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-	:root {
-		/* --rosewater: rgb(245, 224, 220); */
-		/* --flamingo: rgb(242, 205, 205); */
-		/* --pink: rgb(245, 194, 231); */
-		/* --mauve: rgb(203, 166, 247); */
-		/* --red: rgb(243, 139, 168); */
-		/* --maroon: rgb(235, 160, 172); */
-		/* --peach: rgb(250, 179, 135); */
-		/* --yellow: rgb(249, 226, 175); */
-		/* --green: rgb(166, 227, 161); */
-		/* --teal: rgb(148, 226, 213); */
-		/* --sky: rgb(137, 220, 235); */
-		/* --blue: rgb(137, 180, 250); */
-		/* --sapphire: rgb(116, 199, 236); */
-		/* --lavender: rgb(180, 190, 254); */
-		--base: rgb(30, 30, 46);
-		/* --mantle: rgb(24, 24, 37); */
-		/* --crust: rgb(17, 17, 27); */
-		--text: rgb(205, 214, 244);
-	}
-
-	.background {
-		background-color: var(--base);
-	}
 
 	.white_text {
 		color: var(--text);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,15 @@ const config = {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		require('@catppuccin/tailwindcss')({
+			// prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+			// default is `false`, which means no prefix
+			prefix: 'ctp',
+			// which flavour of colours to use by default, in the `:root`
+			defaultFlavour: 'mocha'
+		})
+	]
 };
 
 module.exports = config;
