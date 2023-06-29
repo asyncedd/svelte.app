fd22d44024e987b9962152400d2551c6e1c10fa4
asyncedd <isynqquwu@proton.me>
2023-06-29 16:36:31 +0900

    feat(tailwindcss): specify line-height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="white_text">
 	<body class="background">
-		<h1 class="mx-auto text-center text-5xl antialiased">hi</h1>
+		<h1 class="mx-auto text-center text-5xl antialiased leading-relaxed">hi</h1>
 	</body>
 </div>
 
