a10478a004c3416387993ff10e28c26b1fcf579a
asyncedd <isynqquwu@proton.me>
2023-07-08 11:33:45 +0900

    feat: Componentize the wave styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Hackerman from '../components/hackerman.svelte';
+  import Wave from '../components/wave.svelte';
   import { onMount, afterUpdate } from 'svelte';
 
   let showPopup: number = 0;
@@ -97,19 +98,7 @@
     A erat nam at lectus. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Consectetur lorem donec massa sapien faucibus et molestie ac. Dui accumsan sit amet nulla. Vulputate dignissim suspendisse in est. Pellentesque habitant morbi tristique senectus et netus et. Ut tristique et egestas quis ipsum suspendisse ultrices. Convallis tellus id interdum velit laoreet id donec ultrices. At quis risus sed vulputate odio. Non arcu risus quis varius quam quisque. Convallis tellus id interdum velit laoreet id donec ultrices.
 
     Vulputate ut pharetra sit amet aliquam id. Id diam maecenas ultricies mi eget mauris pharetra et. At erat pellentesque adipiscing commodo elit. Morbi leo urna molestie at elementum eu. Laoreet suspendisse interdum consectetur libero. Facilisis leo vel fringilla est ullamcorper eget. Scelerisque eleifend donec pretium vulputate sapien nec. Ornare lectus sit amet est. Donec adipiscing tristique risus nec feugiat in. Tempus imperdiet nulla malesuada pellentesque elit eget. Malesuada proin libero nunc consequat interdum. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Vulputate dignissim suspendisse in est ante in nibh mauris. Sed vulputate mi sit amet mauris. Facilisis leo vel fringilla est ullamcorper.</p>
-  <div class="wave">
-    <svg
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1200 120"
-      preserveAspectRatio="none"
-      >
-      <path
-        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-        class="shape-fill"
-      />
-    </svg>
-  </div>
+    <Wave />
 </body>
 
 <style>
@@ -165,35 +154,5 @@
 
   .material-symbols-outlined {
     font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 45;
-  }
-
-  .wave {
-    position: absolute;
-    top: 29.3rem;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    line-height: 0;
-  }
-
-  .wave svg {
-    position: relative;
-    display: block;
-    width: calc(100% + 1.3px);
-    height: 157px;
-    animation: wave-animation 10s ease-in-out infinite alternate;
-  }
-
-  @keyframes wave-animation {
-  0% {
-    width: calc(100% + 1px);
-  }
-  100% {
-    width: calc(100% + 100px);
-  }
-  }
-
-  .wave .shape-fill {
-    fill: #89b4fa;
   }
 </style>
